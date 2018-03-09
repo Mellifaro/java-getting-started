e97beedab62b2bcf532bd8d1674f20bfe05d6f6e
Viktor Skapoushchenko <viktor_skapoushchenko@epam.com>
2018-03-09 14:56:56 +0200

    Added junit dependency to pom. Created failed test

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Viktor_Skapoushchenk on 3/9/2018.
+ */
+public class MainTest {
+
+    @Test
+    public void testBuildCI(){
+        assertTrue(false);
+    }
+}
